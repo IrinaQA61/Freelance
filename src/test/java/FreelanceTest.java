8fc763a104fd6ae75d08b158fb99f22dd0fcd44d
Neto test <irina.plotnikova72@mail.ru>
2023-05-03 16:34:04 +0500

    first commit

---
+++ src/test/java/FreelanceTest.java
@@ -0,0 +1,33 @@
+package ru.netology.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+
+public class FreelanceTest {
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/Freelance.csv")
+    public void testCashFreelance(int expected, int income, int expenses, int threshold) {
+        Freelance service = new Freelance();
+        //int expected = 3;
+        //int income = 10_000; // доход от работы
+        //int expenses = 3_000; // обязательные месячные траты
+        //int threshold = 20_000; // есть возможность отдохнуть
+        int actual = service.calculate(income, expenses, threshold);
+        Assertions.assertEquals(expected, actual);
+    }
+    //   @Test
+    //  public void testCashFreelanceRich() {
+    //  Freelance service = new Freelance();
+    //  int expected = 2;
+    //  int income = 100_000; // доход от работы
+    //  expenses = 60_000; // обязательные месячные траты
+    //  int threshold = 150_000; // есть возможность отдохнуть
+    //  int actual = service.calculate(income, expenses, threshold);
+
+    //    Assertions.assertEquals(expected, actual);
+    //}
+
+}
